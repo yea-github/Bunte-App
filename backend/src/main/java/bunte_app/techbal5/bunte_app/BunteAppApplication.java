90ef8e2af5935b9cfd7fb0aac62d674bd264cf4f
Muhammad Yeasin <yea.dev.de@gmail.com>
2026-04-29 12:37:22 +0200

    docker compose, springboot, react

---
+++ backend/src/main/java/bunte_app/techbal5/bunte_app/BunteAppApplication.java
@@ -0,0 +1,13 @@
+package bunte_app.techbal5.bunte_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunteAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/bunte_app/techbal5/bunte_app/BunteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package bunte_app.techbal5.bunte_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
