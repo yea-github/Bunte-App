0e1aa2b1a86b8210e31756764310c5081d970c69
Muhammad Yeasin <yea.dev.de@gmail.com>
2026-05-20 17:33:51 +0200

    Login URL fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useMemo, useState } from 'react';
 import './App.css';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8081/login';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8081';
 
 function App() {
   const [username, setUsername] = useState('');
